bc0fcfb8a8e3066b8df36aa60e22ddcfed5bf85f
zhangzhong <im.zhong@outlook.com>
2025-09-16 13:25:03 +0000

    config several tools

--- tests/unit/api/test_api.py
+++ tests/unit/api/test_api.py
@@ -1,11 +1,18 @@
 # test_app.py
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
+
 from eduagent.api import api
 
 client = TestClient(api)
 
 
-def test_hello_endpoint():
+def test_hello_endpoint() -> None:
     response = client.get("/hello")
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == "hello"
+
+
+async def test_async() -> None:
+    assert True
