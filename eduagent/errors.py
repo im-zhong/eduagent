8c14728ed42c320a80465fc41be7de8df482a0ab
zhangzhong <im.zhong@outlook.com>
2025-09-22 13:24:47 +0000

    DEMO: add some errors

--- eduagent/errors.py
+++ eduagent/errors.py
@@ -1,2 +1,6 @@
 # TODO(zhangzhong): errors could not be easily extracted from the entire codebase
 # I need to refactor other components first, and extract errors gradually.
+
+
+class EduAgentError(Exception):
+    """Base class for all EduAgent errors."""
